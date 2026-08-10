774da246ec9d967a0a555aa9441590371c06e9f7
Lokesh Naga Sai Tungala <lokeshnagasaitungala79@gmail.com>
2026-08-10 22:13:13 +0530

    EDA and Problems Statement

--- src/exception.py
+++ src/exception.py
@@ -1,12 +1,9 @@
 import sys
-
-
 def error_message_detail(error, error_detail: sys):
     _, _, exc_tb = error_detail.exc_info()
-
     file_name = exc_tb.tb_frame.f_code.co_filename
     line_number = exc_tb.tb_lineno
-
+    
     error_message = (
         f"Error occurred in Python script: [{file_name}] "
         f"line number: [{line_number}] "
